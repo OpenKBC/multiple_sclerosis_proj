0875d7ad5eaeffc2a770d52f72b78efe0cf1799a
Jun <swiri021@gmail.com>
2021-09-14 02:33:42 -0500

    Added docker-compose, dockerfile for jupyter notebook setting

---
+++ notebook/installers/installer_Rpackage.R
@@ -0,0 +1,5 @@
+### install DESeq2, tximport packages from Bioconductor
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager", repos='http://cran.us.r-project.org')
+  BiocManager::install("DESeq2")
+  BiocManager::install("tximport")
