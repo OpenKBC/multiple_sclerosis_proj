1953edd4379d20e857c7065577c34d251e754e20
Jun <swiri021@gmail.com>
2021-10-20 16:52:39 -0500

    Minor fix

--- aws_module/batch_deployment/deg_pipeline_batch/batch_module_singleJob.sh
+++ aws_module/batch_deployment/deg_pipeline_batch/batch_module_singleJob.sh
@@ -5,12 +5,14 @@
 
 echo "Creating compute environment.."
 aws batch create-compute-environment --compute-environment-name deg-pipeline-env \
---type MANAGED --compute-resources type=FARGATE,maxvCpus=4,securityGroupIds=sg-08946d1b26a30d376,subnets=[subnet-46231822,subnet-5c5f8b53]
+--type MANAGED --compute-resources type=FARGATE,maxvCpus=8,securityGroupIds=sg-08946d1b26a30d376,subnets=[subnet-46231822,subnet-5c5f8b53]
+
+sleep 3
 
 echo "Creating job queue.."
 aws batch create-job-queue --job-queue-name deg-pipeline-queue --compute-environment-order order=1,computeEnvironment=deg-pipeline-env --priority 100
 
-echo "Creating job1.."
+echo "Creating job.."
 aws batch register-job-definition --job-definition-name deg-pipeline-job --platform-capabilities FARGATE \
 --type container --container-properties file://container_configure.json
 
